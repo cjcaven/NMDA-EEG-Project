b2283cb9c8eb0b94e7df86cace782bbbb830d187
Denise Kittelmann <110939772+dkittelmann@users.noreply.github.com>
2023-08-28 10:27:34 +0200

    Update fieldtripeeg.m

--- fieldtripeeg.m
+++ fieldtripeeg.m
@@ -3,24 +3,29 @@
 
 % Load data
 filename = './bids/sub-/eeg/sub-_task-oddballauditoryparadigm_eeg.bdf'; 
-trialdef = load('trialdef.mat');
+%trialdef = load('trialdef.mat');
 
-% Define Trials 
+% Define standard & rare trials
 cfg = [];
 cfg.dataset = filename;
-cfg.trialfun = 'ft_trialfun_general';
-cfg.trialdef.eventvalue = [1 3];
-fg.trialdef.prestim        = 1; % in sec
-cfg.trialdef.poststim       = 2; % in sec
+cfg.trialdef.eventvalue = 65152;
+cfg.trialdef.prestim        = 0.04; % in sec
+cfg.trialdef.poststim       = 0.15; % in sec
+cfg.trl_standard = ft_definetrial(cfg); %standard trials
 
-cfg = ft_definetrial(cfg);
+cfg.trialdef.eventvalue = 65216;
+cfg.trl_rare = ft_definetrial(cfg); %rare trials
+
+
+% Epoching
+data_standard = ft_redefinetrial(cfg.trl_standard);
+data_rare = ft_redefinetrial(cfg.trl_rare);
 
 
 % Custom montage
-cfg.montage.labelold = {'EXG1' 'EXG2' 'EXG3'};
-cfg.montage.labelnew = {'HEOG' 'VEOG' 'EXG3'};
-data_exg = ft_preprocessing(cfg);
-
+%cfg.montage.labelold = {'EXG1' 'EXG2' 'EXG3'};
+%cfg.montage.labelnew = {'HEOG' 'VEOG' 'EXG3'};
+%data_exg = ft_preprocessing(cfg);
 
 % High-pass filtering
 
@@ -40,12 +45,8 @@ cfg.lpfilter = 'yes';
 cfg.lpfreq = 30; 
 cfg.lpfilttype = 'firws';
 
-% Epoching
-cfg = [];
-cfg.trl = trialdef.trl;
-data_all = ft_redefinetrial(cfg,data_all);
-
 % Artefact handling 
+cfg.channel = 'all';
 cfg.artfctdef.zvalue.cutoff      = 4;
 cfg.artfctdef.zvalue.trlpadding  = 0;
 cfg.artfctdef.zvalue.artpadding  = 0.1;
@@ -68,15 +69,8 @@ cfg = ft_rejectartifacts(cfg);
 
 
 % Averaging
-cfg.contionous = 'yes'; 
-cfg.preproc.demean = 'yes';
-cfg.preproc.baselinewindow = [-0.1 0];
-
-
-% Re-refrencing
-cfg.method = 'avg'; 
-cfg.reref = 'yes'; 
-cfg.refchannel = 'all'; 
+cfg = [];
+cfg.robust = 'yes'; 
 
 data_clean = ft_preprocessing(cfg);
 
@@ -85,7 +79,6 @@ cfg = [];
 cfg.trials = find(data_clean.trials == 1);
 standard = ft_timelockanalysis(cfg,data_clean);
 
-
 cfg = []; 
 cfg.trials = find(data_clean.trials == 3);
 rare = ft_timelockanalysis(cfg,data_clean);
@@ -93,9 +86,7 @@ rare = ft_timelockanalysis(cfg,data_clean);
 % Plot ERP
 cfg = [];
 cfg.channel = 'C23';
-ft_singleplotER(cfg, standard, rare)
-
-
+figure; ft_singleplotER(cfg, standard, rare)
 
 
 
